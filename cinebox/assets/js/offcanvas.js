8f01e041348ad2b64ef919c7d0ab4ebb39fe1fc3
Ryan <ryanlimagermano@gmail.com>
2025-03-28 11:15:14 -0300

    Delete cinebox directory

--- cinebox/assets/js/offcanvas.js
+++
@@ -1,9 +0,0 @@
-function abrirNav(){
-    const menu = document.getElementById("offcanvas")
-    menu.style.width = "250px"
-}
-
-function fecharnav(){
-    const menu = document.getElementById("offcanvas")
-    menu.style.width = "0"
-}
